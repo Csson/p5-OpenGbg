2cab28cda45f43f22e2f4480e9088d9838fc41e7
Erik Carlsson <info@code301.com>
2014-12-10 21:32:53 +0100

    "Init."

---
+++ lib/OpenGbg.pm
@@ -0,0 +1,28 @@
+package OpenGbg;
+
+use strict;
+use warnings;
+use 5.10.1;
+
+
+1;
+
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+OpenGbg - Short intro
+
+=head1 SYNOPSIS
+
+  use OpenGbg;
+
+=head1 DESCRIPTION
+
+OpenGbg is ...
+
+=head1 SEE ALSO
+
+=cut
