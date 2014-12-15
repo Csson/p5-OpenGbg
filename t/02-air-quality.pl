eae35d4c6b81d60506167748a2fa86ffde7ec049
Erik Carlsson <info@code301.com>
2014-12-15 18:10:58 +0100

    added airquality test

---
+++ t/02-air-quality.pl
@@ -0,0 +1,20 @@
+use strict;
+use Test::More;
+use File::HomeDir;
+use Path::Tiny;
+use OpenGbg;
+
+
+my $home = File::HomeDir->my_home;
+my $has_conf_file = path($home)->child('.opengbg.ini')->exists;
+
+if(!$has_conf_file) {
+    plan skip_all => 'You need to get an api key from http://data.goteborg.se/ and put it in ~/.opengbg.ini. See documentation.';
+}
+
+my $open = OpenGbg->new;
+
+is $open->air_quality->latest_measurement->measurement->temperature_unit, 'degrees C', 'Got temperature unit';
+
+done_testing;
+
