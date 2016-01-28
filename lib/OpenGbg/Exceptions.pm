e3536591a5656f44132efb7b4cd6af1278ca88a0
Erik Carlsson <info@code301.com>
2016-01-28 13:49:07 +0000

    New exception style

---
+++ lib/OpenGbg/Exceptions.pm
@@ -0,0 +1,19 @@
+use 5.10.1;
+use strict;
+use warnings;
+
+package OpenGbg::Exceptions;
+
+# VERSION
+
+use Throwable::SugarFactory;
+
+exception BadResponseFromService
+       => 'Failed to get response from service'
+       => has => [service => (is => 'ro')]
+       => has => [url => (is => 'ro')]
+       => has => [status => (is => 'ro')]
+       => has => [reason => (is => 'ro')]
+       ];
+
+1;
